fdaa47e4663d8df55b325fa2e7266060366134cd
Christian Romero <christianromero.work@gmail.com>
2022-08-03 19:56:42 -0500

    Add dayjs CDN; implement history button render; add conversion process for unix timestamp

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,37 +8,109 @@ var currentHumid = document.querySelector("#current-humid");
 var currentUvi = document.querySelector("#current-uvi");
 var forecastContainer = document.querySelector("#forecast-container");
 
+const API_KEY = "2bb4ff4af24c07e562a48a0e7543c161";
+
 var city = "";
 var weatherData = "";
+var searches = [];
+var storedSearches;
 
 function apiCall() {
   $.ajax({
-    url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=2bb4ff4af24c07e562a48a0e7543c161`,
+    url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${API_KEY}`,
     method: "GET",
-  }).then(function (response) {
-    console.log(response);
-    weatherData = response;
-    renderWeather();
-    renderForecast();
+  }).then(function (response1) {
+    console.log("response1", response1);
+    cityName.textContent = response1.name;
+    storeData(response1.name);
+
+    $.ajax({
+      url: `https://api.openweathermap.org/data/2.5/onecall?lat=${response1.coord.lat}&lon=${response1.coord.lon}&units=imperial&appid=${API_KEY}`,
+      method: "GET",
+    }).then(function (response2) {
+      console.log("response2", response2);
+      weatherData = response2;
+      renderWeather();
+      renderForecast();
+      renderHistory();
+    });
   });
 }
 
 function renderWeather() {
-  cityName.textContent = weatherData.name;
+  currentTemp.textContent = `Temperature: ${weatherData.current.temp} ° F`;
+  currentWind.textContent = `Wind Speed: ${weatherData.current.wind_speed} MPH`;
+  currentHumid.textContent = `Humidity: ${weatherData.current.humidity} %`;
+  currentUvi.textContent = `UV Index: ${weatherData.current.uvi}`;
 }
 
 function renderForecast() {
   // Clears current forecast
   forecastContainer.innerHTML = "";
 
-  var forecastCard = document.createElement("div");
-  forecastCard.classList.add("forecast-card");
-  forecastCard.setAttribute("style", "background-color:darkblue");
-  forecastContainer.appendChild(forecastCard);
+  for (i = 0; i < 5; i++) {
+    var forecastCard = document.createElement("div");
+    forecastCard.classList.add("forecast-card");
+    forecastCard.setAttribute("style", "background-color:darkblue");
+    forecastContainer.appendChild(forecastCard);
 
-  var cardDate = document.createElement("p");
-  cardDate = document.setAttribute("style", "color:white");
-  cardDate.textContent = weatherData.daily;
+    var cardDate = document.createElement("p");
+    cardDate.setAttribute("style", "color:white");
+    var unixDate = dayjs.unix(weatherData.daily[i].dt);
+    cardDate.textContent = unixDate.format("MM/DD/YYYY");
+
+    var cardTemp = document.createElement("p");
+    cardTemp.setAttribute("style", "color:white");
+    cardTemp.textContent = `Temperature: ${weatherData.daily[i].temp.day} ° F`;
+
+    var cardWind = document.createElement("p");
+    cardWind.setAttribute("style", "color:white");
+    cardWind.textContent = `Wind Speed: ${weatherData.daily[i].wind_speed} MPH`;
+
+    var cardHumid = document.createElement("p");
+    cardHumid.setAttribute("style", "color:white");
+    cardHumid.textContent = `Humidity: ${weatherData.daily[i].humidity} %`;
+
+    forecastCard.appendChild(cardDate);
+    forecastCard.appendChild(cardTemp);
+    forecastCard.appendChild(cardWind);
+    forecastCard.appendChild(cardHumid);
+  }
+}
+
+function storeData(data) {
+  console.log("data", data);
+  searches.push(data);
+  localStorage.setItem("city", JSON.stringify(searches));
+  loadData();
+}
+
+function loadData() {
+  storedSearches = JSON.parse(localStorage.getItem("city"));
+}
+
+function renderHistory() {
+  recentCities.innerHTML = "";
+
+  if (storedSearches) {
+    for (i = 0; i < storedSearches.length; i++) {
+      historyButton = document.createElement("button");
+      historyButton.textContent = storedSearches[i];
+      recentCities.appendChild(historyButton);
+    }
+
+    historyButton.addEventListener("click", function (event) {
+      event.preventDefault();
+      city = historyButton.textContent;
+      apiCall();
+    });
+  }
+}
+
+// Initialization function ran on page load
+function init() {
+  loadData();
+  renderHistory();
 }
 
 function eventHandler(event) {
@@ -51,3 +123,5 @@ function eventHandler(event) {
 
 searchField.addEventListener("submit", eventHandler);
 searchButton.addEventListener("click", eventHandler);
+
+init();
